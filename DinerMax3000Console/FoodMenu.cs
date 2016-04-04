4afe4fb325e29a08a11b7d4abe2440c5dc0c70db
mattoes <matthewstephens505@gmail.com>
2016-04-04 14:01:23 +0100

    "Orders"

---
+++ DinerMax3000Console/FoodMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000Console
+{
+    public class FoodMenu:Menu //Inheritance
+    {
+        public string HospitalDirections;
+
+    }
+}
--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -10,10 +10,22 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
-            Menu summerMenu = new Menu();
+            FoodMenu summerMenu = new FoodMenu();
             summerMenu.Name = "Summer Menu";
             summerMenu.AddMenuItem("Salmon", "Fresh Norweigan Salmon with Sandefjord butter.", 25.50);
             summerMenu.AddMenuItem("Taco", "All Norweigans eat taco on Fridays", 10);
+            summerMenu.HospitalDirections = "Right around the corner";
+
+
+            DrinkMenu outsideDrinks = new DrinkMenu();
+            outsideDrinks.Disclaimer = "Do not drink and code.";
+            outsideDrinks.AddMenuItem("Virgin Cuba Libre", "A classic", 10);
+            outsideDrinks.AddMenuItem("Screwdriver", "Makes you hammered.", 15);
+
+
+            Order hungryGuestOrder = new Order();
+
+            
 
            
         }
